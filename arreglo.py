8273d09045960b1c0d7558f624a55497ecbba8f9
esme-250253 <250253@utxicotepec.edu.mx>
2026-09-15 20:39:58 -0600

    Ejemplo de arreglo.py

---
+++ arreglo.py
@@ -0,0 +1,14 @@
+# Crear un array (lista) de frutas
+frutas = ["manzana", "pera", "uva"]
+
+# Acceder a un elemento por su índice (empieza en 0)
+print(frutas[0])  # Resultado: manzana
+
+# Modificar un elemento
+frutas[1] = "naranja"
+
+# Agregar un elemento al final
+frutas.append("sandía")
+
+# Conocer la cantidad de elementos
+print(len(frutas))  # Resultado: 4
